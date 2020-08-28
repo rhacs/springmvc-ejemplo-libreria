5de5f547c9db5e456ab1508a77a38699d3d034d0
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-28 13:23:11 -0400

    added table and sequence definitions for categorias
    added unique constraint to publicadores

--- schema.sql
+++ schema.sql
@@ -26,8 +26,30 @@ CREATE TABLE lib_publicadores (
     nombre NVARCHAR2(150) NOT NULL,
 
     -- Llave primaria
-    CONSTRAINT lib_publicadores_pk PRIMARY KEY (publicador_id)
+    CONSTRAINT lib_publicadores_pk PRIMARY KEY (publicador_id),
+
+    -- Columnas únicas
+    CONSTRAINT lib_publicadores_uq UNIQUE (nombre)
 );
 
 -- Secuencia
 CREATE SEQUENCE lib_publicadores_seq START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE ORDER;
+
+----------------------------------------------------------------------------------------------------
+-- Tabla: lib_categorias
+----------------------------------------------------------------------------------------------------
+
+-- Tabla
+CREATE TABLE lib_categorias (
+    categoria_id NUMBER NOT NULL,
+    nombre NVARCHAR2(50) NOT NULL,
+
+    -- Llave primaria
+    CONSTRAINT lib_categorias_pk PRIMARY KEY (categoria_id),
+
+    -- Columnas únicas
+    CONSTRAINT lib_categorias_uq UNIQUE (nombre)
+);
+
+-- Secuencia
+CREATE SEQUENCE lib_categorias_seq START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE ORDER;
