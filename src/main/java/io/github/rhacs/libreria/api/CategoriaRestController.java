596e5b9c965cd3407b39300a8cf95af998d6fb3b
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-28 18:01:05 -0400

    added method eliminarRegistro

--- src/main/java/io/github/rhacs/libreria/api/CategoriaRestController.java
+++ src/main/java/io/github/rhacs/libreria/api/CategoriaRestController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -155,7 +156,7 @@ public class CategoriaRestController {
             Optional<Categoria> existente = categoriasRepositorio.findByNombre(categoria.getNombre());
 
             // Verificar si existe y si los identificadores son distintos
-            if (existente.isPresent() && existente.get().getId() != categoria.getId()) {
+            if (existente.isPresent() && !existente.get().getId().equals(categoria.getId())) {
                 // Preparar respuesta
                 ErrorResponse response = prepararError("nombre", "El nombre está en uso", "Categoria",
                         categoria.getNombre(), HttpStatus.CONFLICT);
@@ -177,6 +178,34 @@ public class CategoriaRestController {
 
         // Lanzar excepción
         throw new InconsistenciaParametrosException(response);
+    }
+
+    // Solicitudes DELETE
+    // -----------------------------------------------------------------------------------------
+
+    /**
+     * Elimina una {@link Categoria} del repositorio
+     * 
+     * @param id identificador numérico de la {@link Categoria}
+     */
+    @DeleteMapping(path = "/{id:^[0-9]+$}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void eliminarRegistro(@PathVariable Long id) {
+        // Buscar información de la categoría
+        Optional<Categoria> categoria = categoriasRepositorio.findById(id);
+
+        // Verificar si existe
+        if (categoria.isPresent()) {
+            // Eliminar categoria
+            categoriasRepositorio.delete(categoria.get());
+        } else {
+            // Preparar respuesta
+            ErrorResponse response = prepararError("id", "La Categoría solicitada no existe", "Categoria", id,
+                    HttpStatus.NOT_FOUND);
+
+            // Lanzar excepcion
+            throw new ElementoNoExisteException(response);
+        }
     }
 
 }
