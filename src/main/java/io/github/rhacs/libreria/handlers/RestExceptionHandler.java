4c21c7ce19199c867b372c603be39a1f45f3f0cd
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-31 17:46:08 -0400

    added handler for ConstraintViolationException

--- src/main/java/io/github/rhacs/libreria/handlers/RestExceptionHandler.java
+++ src/main/java/io/github/rhacs/libreria/handlers/RestExceptionHandler.java
@@ -1,5 +1,8 @@
 package io.github.rhacs.libreria.handlers;
 
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -47,6 +50,30 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { InconsistenciaParametrosException.class })
     protected ResponseEntity<ErrorResponse> handleInconsistenciaParametros(InconsistenciaParametrosException e) {
         return ResponseEntity.status(e.getResponse().getStatusCode()).body(e.getResponse());
+    }
+
+    /**
+     * Maneja las excepciones de validación {@link ConstraintViolationException}
+     * 
+     * @param e objeto {@link ConstraintViolationException} que contiene la
+     *          información de la excepción
+     * @return un objeto {@link ResponseEntity} con la respuesta a la solicitud
+     */
+    @ExceptionHandler(value = { ConstraintViolationException.class })
+    protected ResponseEntity<ErrorResponse> handleConstraintViolation(ConstraintViolationException e) {
+        // Estado HTTP
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        // Crear respuesta
+        ErrorResponse response = new ErrorResponse();
+        response.setStatusCode(status.value());
+        response.setMessage("Error de validación");
+
+        // Agregar errores de validación
+        e.getConstraintViolations().forEach(response::agregarError);
+
+        // Enviar respuesta
+        return ResponseEntity.status(status).body(response);
     }
 
 }
