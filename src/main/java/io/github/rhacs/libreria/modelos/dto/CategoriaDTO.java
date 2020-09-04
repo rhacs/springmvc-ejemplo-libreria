d71d45befb3452f4ade5406a91d2f179895fe73f
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-09-03 21:44:26 -0400

    added dto for Categoria

---
+++ src/main/java/io/github/rhacs/libreria/modelos/dto/CategoriaDTO.java
@@ -0,0 +1,82 @@
+package io.github.rhacs.libreria.modelos.dto;
+
+import java.util.Objects;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class CategoriaDTO {
+
+    // Atributos
+    // -----------------------------------------------------------------------------------------
+
+    private Long id;
+
+    @NotEmpty
+    @Size(max = 50)
+    private String nombre;
+
+    // Constructores
+    // -----------------------------------------------------------------------------------------
+
+    public CategoriaDTO() {
+
+    }
+
+    public CategoriaDTO(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    // Getters
+    // -----------------------------------------------------------------------------------------
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Setters
+    // -----------------------------------------------------------------------------------------
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Herencias (Object)
+    // -----------------------------------------------------------------------------------------
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (obj == null)
+            return false;
+
+        if (getClass() != obj.getClass())
+            return false;
+
+        CategoriaDTO other = (CategoriaDTO) obj;
+
+        return Objects.equals(id, other.id) || Objects.equals(nombre, other.nombre);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("CategoriaDTO [id=%s, nombre=%s]", id, nombre);
+    }
+
+}
