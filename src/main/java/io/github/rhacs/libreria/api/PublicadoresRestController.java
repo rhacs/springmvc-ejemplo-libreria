fb47b775d60576ae59c69d0734d8167a3ede3c87
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-09-03 22:11:17 -0400

    added method editarRegistro

--- src/main/java/io/github/rhacs/libreria/api/PublicadoresRestController.java
+++ src/main/java/io/github/rhacs/libreria/api/PublicadoresRestController.java
@@ -13,12 +13,15 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.rhacs.libreria.Metodos;
 import io.github.rhacs.libreria.excepciones.ElementoNoExisteException;
+import io.github.rhacs.libreria.excepciones.InconsistenciaParametrosException;
 import io.github.rhacs.libreria.excepciones.ViolacionRestriccionUnicaException;
 import io.github.rhacs.libreria.modelos.ErrorResponse;
 import io.github.rhacs.libreria.modelos.Publicador;
@@ -115,6 +118,53 @@ public class PublicadoresRestController {
         pub = publicadoresRepositorio.save(pub);
 
         // Devolver objeto
+        return pub;
+    }
+
+    // Solicitudes PUT
+    // -----------------------------------------------------------------------------------------
+
+    /**
+     * Edita la información de un registro existente
+     * 
+     * @param id         identificador numérico del {@link Publicador}
+     * @param publicador objeto {@link Publicador} que contiene la información a
+     *                   actualizar
+     * @return un objeto {@link Publicador}
+     */
+    @PutMapping(path = "/{id:^[0-9]+$}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Publicador editarRegistro(@PathVariable Long id, @RequestBody @Valid PublicadorDTO publicador) {
+        // Verificar si los identificadores no coinciden
+        if (!id.equals(publicador.getId())) {
+            // Preparar error
+            ErrorResponse response = Metodos.prepararError("id", "Los identificadores no coinciden",
+                    Publicador.class.getSimpleName(), new Object[] { id, publicador.getId() }, HttpStatus.BAD_REQUEST);
+
+            // Lanzar excepción
+            throw new InconsistenciaParametrosException(response);
+        }
+
+        // Buscar información del registro existente por nombre
+        Optional<Publicador> existente = publicadoresRepositorio.findByNombre(publicador.getNombre());
+
+        // Verificar si existe y el identificador sea distinto
+        if (existente.isPresent() && !id.equals(existente.get().getId())) {
+            // Preparar error
+            ErrorResponse response = Metodos.prepararError("nombre", "El nombre está en uso",
+                    Publicador.class.getSimpleName(), publicador.getNombre(), HttpStatus.CONFLICT);
+
+            // Lanzar excepción
+            throw new ViolacionRestriccionUnicaException(response);
+        }
+
+        // Convertir DTO a Entidad
+        Publicador pub = new Publicador(publicador.getId(), publicador.getNombre());
+
+        // Guardar cambios en el repositorio
+        pub = publicadoresRepositorio.save(pub);
+
+        // Devolver objeto con los cambios realizados
         return pub;
     }
 
