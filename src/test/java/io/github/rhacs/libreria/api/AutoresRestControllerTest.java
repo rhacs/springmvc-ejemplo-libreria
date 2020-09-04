7a11fe751b8cb25168e782f6a2ee6c5cd27b937f
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-09-04 00:41:32 -0400

    added unit tests for mostrarUno

--- src/test/java/io/github/rhacs/libreria/api/AutoresRestControllerTest.java
+++ src/test/java/io/github/rhacs/libreria/api/AutoresRestControllerTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import io.github.rhacs.libreria.excepciones.ElementoNoExisteException;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/appServlet-context.xml",
         "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
@@ -48,6 +50,44 @@ class AutoresRestControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 // Esperar que el json devuelto sea un listado de objetos
                 .andExpect(jsonPath("$").isArray())
+                // Imprimir por consola
+                .andDo(print());
+    }
+
+    // mostrarUno()
+    // -----------------------------------------------------------------------------------------
+
+    @Test
+    void mostrarUnoDeberiaDevolverUnObjeto() throws Exception {
+        // Identificador numérico del Autor
+        Long id = 2L;
+
+        mvc
+                // Realizar petición GET a la API
+                .perform(get(API_AUTORES_ID, id))
+                // Esperar que el estado de la respuesta sea 200 (OK)
+                .andExpect(status().isOk())
+                // Esperar que el tipo de contenido de la respuesta sea "application/json"
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // Esperar que el json devuelto tenga un atributo "nombreCompleto" con el valor
+                // "Stieg Larsson"
+                .andExpect(jsonPath("$.nombreCompleto").value("Stieg Larsson"))
+                // Imprimir por consola
+                .andDo(print());
+    }
+
+    @Test
+    void mostrarUnoDeberiaLanzarElementoNoExiste() throws Exception {
+        // Identificador numérico del Autor
+        Long id = 10000L;
+
+        mvc
+                // Realizar petición GET a la API
+                .perform(get(API_AUTORES_ID, id))
+                // Esperar que el estado de la respuesta sea 404 (NOT_FOUND)
+                .andExpect(status().isNotFound())
+                // Esperar que la excepción lanzada sea ElementoNoExisteException
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ElementoNoExisteException))
                 // Imprimir por consola
                 .andDo(print());
     }
